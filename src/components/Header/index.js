82f6437d12fa569a4f0b411242bb074eae692964
Zeus3hd <dafuq.ahd@gmail.com>
2019-10-26 11:40:02 +0300

    add header component with background image and after pseudo element as an overlay

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import headerImage from "../../images/header.jpg";
+const Wrapper = styled.header`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: url(${headerImage}) no-repeat;
+  background-size: cover;
+  ::after {
+    content: "";
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.5);
+    position: absolute;
+  }
+`;
+export default function Header() {
+  return <Wrapper></Wrapper>;
+}
