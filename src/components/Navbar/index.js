ca30bd8102f49148ec20097489037f8662dfe45a
Zeus3hd <dafuq.ahd@gmail.com>
2019-10-26 12:36:58 +0300

    working responsive media querry for navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,6 +6,7 @@ import CH from "../../images/ch.png";
 import RU from "../../images/ru.png";
 import TR from "../../images/tr.png";
 import { FaCaretDown } from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
 const Wrapper = styled.nav`
   position: absolute;
   top: 0;
@@ -27,6 +28,8 @@ const Wrapper = styled.nav`
       list-style-type: none;
       display: grid;
       grid-template-columns: repeat(5, 1fr);
+      align-content: center;
+
       li {
         margin: 0.5rem;
       }
@@ -62,6 +65,37 @@ const Wrapper = styled.nav`
         text-decoration: none;
         color: #555555;
         font-weight: bold;
+      }
+    }
+  }
+  @media (min-width: 801px) {
+    #bottom-bar button {
+      display: none;
+    }
+  }
+  @media (max-width: 800px) {
+    #bottom-bar ul {
+      display: none;
+    }
+    #bottom-bar button {
+      justify-self: end;
+      width: 70px;
+      padding: 0;
+      margin: 0;
+      font-size: 1.25rem;
+      background: none;
+      border: none;
+      color: #555555;
+    }
+    #top-bar {
+      #top-bar-contact {
+        display: none;
+      }
+      #currencies {
+        grid-column: 3/3;
+        > li {
+          margin: 0.5rem;
+        }
       }
     }
   }
@@ -139,6 +173,9 @@ export default function Navbar() {
             <a href="#dead">Contact</a>
           </li>
         </ul>
+        <button>
+          <FaBars />
+        </button>
       </div>
     </Wrapper>
   );
